a81fca75d760d77269b83ee08fd0ed775acabbf6
Pinponik <256419204+Pinponik@users.noreply.github.com>
2026-07-29 12:43:56 +0200

    Continue making rOSox
    Signed-off-by: Pinponik <256419204+Pinponik@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,12 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
+    println!("Hello World{}", "!");
     loop {}
 }
